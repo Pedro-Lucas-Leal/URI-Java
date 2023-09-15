dc46bd732c56822ecefe53de5d45ad00de261f9b
pedrolucas.leal@gmail.com <pedrolucas.leal@gmail.com>
2023-09-14 21:30:04 -0300

    Update 1002.java

--- 1002.java
+++ 1002.java
@@ -5,6 +5,6 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         double raio = sc.nextDouble();
         double area = 3.14159*raio*raio;
-        System.out.printf("A=%.4f",area);
+        System.out.printf("A=%.4f\n",area);
     }
 }
