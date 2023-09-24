a7c56810c6652f266e57a3dc868cd8c6e3c56366
pedrolucas.leal@gmail.com <pedrolucas.leal@gmail.com>
2023-09-24 04:15:29 -0300

    Create 1019.java

---
+++ 1019.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int segundos = sc.nextInt();
+        int minutos=0, horas=0;
+
+        while (segundos>60){
+            segundos=segundos-60;
+            minutos++;
+        }
+        while (minutos>60){
+            minutos=minutos-60;
+            horas++;
+        }
+
+        System.out.println(horas+":"+minutos+":"+segundos);
+    }
+}
